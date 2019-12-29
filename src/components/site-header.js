223108d2c1ab620211cc713e93bd74f47d1ccadc
Calvin Shell <shell.calvin7@gmail.com>
2019-12-28 19:37:29 -0700

    build

--- src/components/site-header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const SiteHeader = () => {
-  return <div>logo here and title and menu too</div>
-}
--- src/components/typography.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const Copy = styled.p``
-
-export const H1 = styled.h1``
-
-export const H2 = styled.h2``
