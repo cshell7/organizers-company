17bed314cd9d95a620ef16b3b03dbb3beac3951e
Calvin Shell <shell.calvin7@gmail.com>
2019-12-28 19:35:07 -0700

    build

---
+++ build/precache-manifest.a867d091e91e1735c0bcabf156ac4b2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aced964ac39c75c21ddb93d2b5337be5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25724dcfac570cc1cbc2",
+    "url": "/static/js/2.98996a50.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.98996a50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e3e4897768c856e286f",
+    "url": "/static/js/main.7d70fc70.chunk.js"
+  },
+  {
+    "revision": "9dd7c82c56dbc824bbbd",
+    "url": "/static/js/runtime-main.ff588589.js"
+  }
+]);
